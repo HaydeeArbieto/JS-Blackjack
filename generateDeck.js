9f2adc7788457bf27d4f7d5c71519f54620ebb09
kjrgreen <kevin.j.r.green@gmail.com>
2016-09-12 20:45:03 +0200

    Created basic function for creating a deck. Deck is an array of objects with two properties: Suit and value. Please copy the code for the arrays "suits" and "values" in the future, as suits contains strange characters. This is to be sure that comparisons when evaluating hands will always be correct.

---
+++ generateDeck.js
@@ -0,0 +1,17 @@
+var generatedeck = function(){
+	var suits = ["♠ ", "♣ ", "♦ ", "♥ "];
+	var values = ["E", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+	var deck = [];
+
+	for(var x=0;x!=suits.length;x++)
+	{
+		for(var y=0;y!=values.length;y++)
+		{
+			deck[deck.length]=suits[x]+values[y];
+			console.log(deck[deck.length-1]);	
+		}
+	}	
+	console.log(deck.length);
+}
+
+generatedeck();
