63ce233f0d76257049c91971490a951760c01be1
HArbietoCortez <arbietohaydee@hotmail.com>
2016-09-13 14:29:32 +0200

    added underscore, more clean upp

--- js/generateDeck.js
+++ js/generateDeck.js
@@ -3,12 +3,12 @@ var generatedeck = function(){
 	var values = ['E', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 	var deck = [];
 
-	for(var x=0;x!==suits.length;x++)
+	for(var x=0; x!==suits.length; x++)
 	{
-		for(var y=0;y!==values.length;y++)
+		for(var y=0; y!==values.length; y++)
 		{
-			deck[deck.length]=suits[x]+values[y];
-			console.log(deck[deck.length-1]);
+			deck.push({suit: suits[x], value: values[y]});
+			console.log(deck[deck.length-1].suit + deck[deck.length-1].value);
 		}
 	}
 	console.log(deck.length);
